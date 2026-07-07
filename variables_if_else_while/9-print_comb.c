9fcf37c22a37d25bf86d03fa984364c97b8366b6
Malakai-lp <gam3rsuknight@gmail.com>
2026-07-07 13:00:16 +1000

    scripts for tasks

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(' ');
+		}
+		i++;
+	}
+
+	putchar ('\n');
+	return (0);
+}
