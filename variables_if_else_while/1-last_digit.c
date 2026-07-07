c7b84d5ad8b1a5734a17f0f0ffafa35f844c44a0
Malakai-lp <gam3rsuknight@gmail.com>
2026-07-07 11:43:16 +1000

    scripts for tasks

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - generates a random number and prints it to terminals
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n;
+	int nl;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	nl = n % 10;
+
+	if (nl > 5)
+	{
+		printf("Last digit of %i is %i is greater than 5\n", n, nl);
+	}
+	else if (n < 0)
+	{
+		printf("Last digit of %i is %i is 0\n", n, nl);
+	}
+	else
+	{
+		printf("Last digit of %i is %i is less than 6 and not 0\n", n, nl);
+	}
+
+	return (0);
+}
