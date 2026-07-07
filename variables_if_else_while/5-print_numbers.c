0d461c7e86bee80fd078503c0d704c8286519d66
Malakai-lp <gam3rsuknight@gmail.com>
2026-07-07 12:35:42 +1000

    scripts for tasks

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%i", i);
+		i++;
+	}
+
+	putchar ('\n');
+	return (0);
+}
