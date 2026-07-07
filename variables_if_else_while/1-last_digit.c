15605b93613db600c497f462f61dededb44afadc
Malakai-lp <gam3rsuknight@gmail.com>
2026-07-07 11:49:58 +1000

    scripts for tasks

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %i is %i and  is less than 6 and not 0\n", n, nl);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, nl);
 	}
 
 	return (0);
