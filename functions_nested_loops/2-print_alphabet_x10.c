f3771614ae980dced37076304bd5e70aa2f69cf4
Malakai <gam3rsuknight@gmail.com>
2026-07-08 15:09:33 +1000

    function for tasks

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet x10  in lower case using ascii
+ *
+ * Return: 0 always
+ */
+void print_alphabet_x10(void)
+{
+	int l = 0;
+	while (l < 10)
+	{
+		int i = 97;
+
+		while (i <= 122)
+		{
+			_putchar (i);
+			i++;
+		}
+
+		_putchar ('\n');
+		l++;
+	}
+	return;
+}
