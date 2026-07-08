d7fff29730ea366bb06c269db1a96e29da92f516
Malakai <gam3rsuknight@gmail.com>
2026-07-08 16:10:30 +1000

    function for tasks

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if input is a letter
+ * @c: input to check if letter
+ * Return: 1 if letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
