a14da15dd9da16ec20efc93ccfaa087e161af4b5
Malakai <gam3rsuknight@gmail.com>
2026-07-08 16:40:45 +1000

    functions for tasks

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - computes absalute value of an integer
+ * @n: input to check
+ * Return: absalute value of input
+ */
+
+int _abs(int n)
+{
+	return ((n < 0) ? -n : n);
+}
