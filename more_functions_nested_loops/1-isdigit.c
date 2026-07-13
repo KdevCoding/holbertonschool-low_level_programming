860e0e9d52666309e621fc40390e569fbbf9eb8f
Malakai <gam3rsuknight@gmail.com>
2026-07-13 13:51:55 +1000

    functions for tasks

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if input is digit 
+ * @c: input to check if digit
+ * Return: 1 if digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
