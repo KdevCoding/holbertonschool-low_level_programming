27e22750f30a62c27f8f3f87e3033a31ab132916
Malakai <gam3rsuknight@gmail.com>
2026-07-13 15:20:26 +1000

    functions for tasks

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Fizz-Buzz test
+ *
+ * Return: always 0
+ */
+
+int  main(void)
+{
+	int i;
+	int f;
+	int t;
+
+	i = 1;
+	f = 0;
+	t = 0;
+	while (i <= 100)
+	{
+		t = i % 3;
+		f = i % 5;
+		if (t == 0)
+			printf("Fizz");
+
+		if (f == 0)
+			printf("Buzz");
+		if (t != 0 && f != 0)
+			printf("%i ", i);
+		i++;
+
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
