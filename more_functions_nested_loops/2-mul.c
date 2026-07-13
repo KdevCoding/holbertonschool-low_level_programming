b8685abae4e7beede4a206876485df971f9b0bf4
Malakai <gam3rsuknight@gmail.com>
2026-07-13 13:59:51 +1000

    functions for tasks

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies a x b
+ * @a: 1st number to multiply
+ * @b: 2nd number to multiply
+ * Return: a x b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
