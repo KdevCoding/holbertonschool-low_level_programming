5397a6c6e3514ba9a938716bbf9a07744f8b32e2
Malakai-lp <gam3rsuknight@gmail.com>
2026-07-14 13:06:08 +1000

    functions for tasks

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char of given string followed by a new line
+ * @str: var to print
+ *
+ *
+ */
+
+void puts2(char *str)
+{
+	int l;
+
+	l = 0;
+	while (str[l] != '\0')
+	{
+		_putchar(str[l]);
+		l += 2;
+	}
+	_putchar('\n');
+}
