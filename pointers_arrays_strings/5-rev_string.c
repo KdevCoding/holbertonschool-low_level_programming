5f249b4ee410f597781745bda07e31a32675524b
Malakai <gam3rsuknight@gmail.com>
2026-07-16 13:22:43 +1000

    function for tasks

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses given var 
+ * @s: var to reverse
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+	int l;
+	int i;
+	char temp;
+	
+	i = 0;
+	l = 0;
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	l--;
+
+	while (l >= i)
+	{
+
+		temp = s[i];
+		s[i] = s[l];
+		s[l] = temp;
+
+		l--;
+		i++;
+	}
+}
