f91e16ac064f8a42b0de6a19633943d6cbfec565
Malakai <gam3rsuknight@gmail.com>
2026-07-16 16:12:35 +1000

    function for tasks

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies  n bytes of src to dest
+ * @src: pointer to read string
+ * @dest: pointer to destnination for string
+ * @n: amount of bytes
+ *
+ * Return: output dest pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int sl;
+	int i;
+
+	i = 0;
+	sl = 0;
+
+	while (src[sl] != '\0')
+	{
+		sl++;
+	}
+
+	while (i <= sl && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
