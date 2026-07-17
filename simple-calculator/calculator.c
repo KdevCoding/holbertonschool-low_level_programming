c46f914ade7f215063929fb0d7322a010424147d
Malakai <gam3rsuknight@gmail.com>
2026-07-17 14:58:45 +1000

    Calculator task

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -10,17 +10,25 @@ int main(void)
 {
 	int active;
 	int input;
+	int Ain;
+	int Bin;
+	int result;
 
 	active = 1;
+	input = 5;
+	Ain = 1;
+	Bin = 1;
+	result = 0;
 
 	while (active)
 	{
-		printf("-----------\n"
-			"1) Add\n"
-			"2) Subtract\n"
-			"3) Multiply\n"
-			"4) Divide\n"
-			"0) Quit\n");
+		printf("\n-----------\n"
+				"1) Add\n"
+				"2) Subtract\n"
+				"3) Multiply\n"
+				"4) Divide\n"
+				"0) Quit\n"
+				"Choice: ");
 		scanf("%d", &input);
 
 		if (input == 0)
@@ -30,6 +38,18 @@ int main(void)
 		}
 		else if (input > 4)
 			printf("Invalid choice\n");
+		else if (input == 1)
+		{
+			printf("A: ");
+			scanf("%d", &Ain);
+			printf("B: ");
+			scanf("%d", &Bin);
+			result = (Ain + Bin);
+			printf("\n###########\n"
+				"Result: %d\n"
+				"###########\n", result);
+
+		}
 
 	}
 	return (1);
