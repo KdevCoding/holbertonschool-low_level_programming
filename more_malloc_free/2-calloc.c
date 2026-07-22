9279b5a4e3da68d9fc103c99cc87fa0b5c2aea34
Malakai <gam3rsuknight@gmail.com>
2026-07-22 14:00:48 +1000

    functions for tasks

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,14 +11,22 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *arr;
+	unsigned char *arr;
+	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	i = 0;
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
 
+	while (i < nmemb * size)
+	{
+		arr[i] = 0;
+		i++;
+	}
 	return (arr);
 }
