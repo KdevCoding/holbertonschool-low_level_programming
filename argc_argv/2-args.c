2af9c9ea21488603e11a0025de9b531e018cebd2
Malakai <gam3rsuknight@gmail.com>
2026-07-31 13:51:16 +1000

    program for tasks

---
+++ argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: amount of arguments
+ * @argv: arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
