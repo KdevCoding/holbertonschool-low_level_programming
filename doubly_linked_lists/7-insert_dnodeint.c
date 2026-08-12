f93c2ca5a840ab1eecd165bbedcb35cc09d540e3
Malakai <gam3rsuknight@gmail.com>
2026-08-12 19:25:44 +1000

    functions for tasks

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,30 @@
 #include <stdio.h>
 #include "lists.h"
 
+
+
+/**
+ * get_dnodeint_at_index - print double linked list
+ * @head: list
+ * @index: what node to return
+ *
+ * Return: number of nodes
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int c;
+
+	c = 0;
+	while (head != NULL && c != index)
+	{
+
+		head = head->next;
+		c++;
+	}
+	return (head);
+}
+
 /**
  * insert_dnodeint_at_index - insert node at index
  * @h: list
@@ -13,44 +37,27 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
-	dlistint_t *head;
-	unsigned int c;
+	dlistint_t *prev_node;
+	dlistint_t *next_node;
 
-	c = 0;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	prev_node = get_dnodeint_at_index(*h, idx - 1);
+	if (prev_node == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
-		printf("Error\n");
 		return (NULL);
-	}
-
 	new->n = n;
-	new->next = NULL;
 
-	if (*h == NULL && c == idx)
-	{
-		*h = new;
-	}
-	else
-	{
-		head = *h;
+	next_node = prev_node->next;
+	new->prev = prev_node;
+	new->next = next_node;
+	prev_node->next = new;
+	if (next_node != NULL)
+		next_node->prev = new;
 
-		while (head->next != NULL && c < idx)
-		{
-			head = head->next;
-			c++;
-		}
-		if (c != idx - 1)
-			head = head->prev;
-
-		if (c < idx - 1)
-		{
-			free(new);
-			return (NULL);
-		}
-		new->prev = head;
-		new->next = head->next;
-		head->next = new;
-	}
 	return (new);
 }
