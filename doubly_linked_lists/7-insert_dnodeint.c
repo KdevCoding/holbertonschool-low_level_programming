915b4d2e4345b55697cd5e2ebaa8f074bfd244b7
Malakai <gam3rsuknight@gmail.com>
2026-08-12 18:18:14 +1000

    functions for tasks

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at index
+ * @h: list
+ * @idx: where to insert node
+ * @n: int val
+ * Return: number of nodes
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int c;
+
+	c = 0;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*h == NULL && c == idx)
+	{
+		*h = new;
+	}
+	else
+	{
+		head = *h;
+
+		while (head->next != NULL && c < idx)
+		{
+			head = head->next;
+			c++;
+		}
+
+		if (c < idx)
+		{
+			free(new);
+			return (NULL);
+		}
+
+		new->prev = head;
+		new->next = head->next;
+		head->next = new;
+	}
+	return (new);
+}
