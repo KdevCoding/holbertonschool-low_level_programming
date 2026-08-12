b76c33601da25566255fb1189529618bb825face
Malakai <gam3rsuknight@gmail.com>
2026-08-12 17:22:34 +1000

    functions for tasks

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print double linked list
+ * @h: list
+ *
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int c;
+
+	c = 0;
+	while (h != NULL)
+	{
+
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
