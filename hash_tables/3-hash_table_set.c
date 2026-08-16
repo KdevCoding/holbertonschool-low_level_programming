8c5e9dc28b3141a2f70ad378ea3a8b8e914a2c84
Malakai <gam3rsuknight@gmail.com>
2026-08-16 20:59:22 +1000

    funcs for tasks

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
  * Return: address of new element
  */
 
-hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+hash_node_t *add_node(hash_node_t *head, const char *key, const char *value)
 {
 	hash_node_t *new;
 
@@ -24,11 +24,11 @@ hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = *head;
+	new->next = head;
 
-	*head = new;
+	head = new;
 
-	return (*head);
+	return (head);
 }
 
 /**
@@ -43,30 +43,32 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *arri;
-	hash_node_t *node;
 
 	if (key == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	arri = ht->array[index];
 
-	node = arri;
-	while (node != NULL)
+
+	while (arri != NULL)
 	{
-		if (node->key == key)
+		if (strcmp(arri->key, key) == 0)
 			break;
-		node = node->next;
+		arri = arri->next;
 	}
 
-	if (node == NULL)
+	if (arri == NULL)
 	{
-		add_node(&arri, key, value);
+		ht->array[index] = add_node(ht->array[index], key, value);
+		printf("value: %s\n", ht->array[index]->value);
 	}
 	else
+	{
 		arri->value = strdup(value);
+		printf("value2: %s\n", ht->array[index]->value);
+	}
 
 	if (arri == NULL)
 		return (0);
-
 	return (1);
 }
