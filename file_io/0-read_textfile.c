26ea7155287dda595fd750e7706d5a1035d1b178
Malakai-lp <gam3rsuknight@gmail.com>
2026-08-18 11:24:43 +1000

    funcs frs tasks

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include "main.h"
 
+/**
+ * read_textfile - writes chars from file
+ * @filename: file to read from
+ * @letters: amounts of chars to read/write
+ *
+ * Return: 0 on error/no write, otherwise amount of chars written
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buff;
-    ssize_t charp;
+	char *buff;
+	int fd;
+	ssize_t charp;
 
-    if (filename == NULL || letters == 0)
-        return (0);
-    
-    buff = malloc(sizeof(char) * letters + 1);
-    if (buff == NULL)
-        return (0);
+	if (filename == NULL || letters == 0)
+		return (0);
 
-    if (read(*filename, buff, letters) == -1)
-        return (-1);
-    
-    charp = write(0, buff, letters);
-    
-    if (charp != (ssize_t)letters)
-        charp = 0;
-    return (charp);
+	buff = malloc(sizeof(char) * letters + 1);
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, 0);
+	charp = read(fd, buff, letters);
+	if (charp == -1)
+		return (0);
+	else if (charp > 0)
+		letters = charp;
+	close(fd);
+	charp = write(0, buff, letters);
+
+	if (charp != (ssize_t)letters)
+		charp = 0;
+	free(buff);
+	return (charp);
 }
